778fc86c961d4a43d3e8a5c59c4971912c3bf0bc
tjijo <148106907+tjijo@users.noreply.github.com>
2023-10-16 12:01:59 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
-read -p "Enter the URL from email: " PRESIGNED_URL
+read -p "Enter the URL from email:https://download2.llamameta.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoiaTl1cjJsYnZqbWxlOGppNXhlOThnZzd5IiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZG93bmxvYWQyLmxsYW1hbWV0YS5uZXRcLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2OTc1NTcyNzl9fX1dfQ__&Signature=BbIwNmlNXjhqSCP0qyu49T8auz%7EcDMkK5kQPIUMfPK6JuYDWf%7EJ1SL2g8nHSUF%7E7mpd-x2okgO0NrwPB0gHOEM4zRfXaOtel1vWxm8yoYJ3vhxoB-OMjOeziPjri4yr7mFTsnqG7h%7E--PACsegCjjAWcdNta71dE0AKKzMFvkcaSjuXZTt%7Eqe0zlpdwSvi-9HVNtZwbCZ-RmHCKCUlJ0nS5NHflkAOUDYs8mT2ndnQDBY29g-t1fEuUHCxEMgn0w81pIcpX3cs5%7EHVO18NNn-t%7EpKvhxBydtoHTERuKiywYtQwSVQEMjDgvCCG0Zm9L7UHX5L7riNKtZM2lOiiKLzg__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=859994752406877 " PRESIGNED_URL
 echo ""
 ALL_MODELS="7b,13b,34b,7b-Python,13b-Python,34b-Python,7b-Instruct,13b-Instruct,34b-Instruct"
 read -p "Enter the list of models to download without spaces ($ALL_MODELS), or press Enter for all: " MODEL_SIZE
